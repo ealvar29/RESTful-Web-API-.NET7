ec50162812699bd4a6232976e5416ee004925d74
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-30 18:17:11 -0500

    added identity and did first migration to Supabase

--- MagicVilla/MagicVilla_VillaApi/Program.cs
+++ MagicVilla/MagicVilla_VillaApi/Program.cs
@@ -1,7 +1,15 @@
+using MagicVilla_VillaApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
