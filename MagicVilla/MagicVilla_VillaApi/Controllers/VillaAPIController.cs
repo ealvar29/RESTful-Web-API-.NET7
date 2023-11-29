4167e6f63fa2433af7a2b10a2b13bdd86205015d
Eduardo <ealvarez@carminaticonsulting.com>
2023-11-29 13:02:25 -0600

    added DTO and Store

---
+++ MagicVilla/MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("id:int")]
+        public VillaDTO GetVilla(int villaId)
+        {
+            return VillaStore.villaList.FirstOrDefault(x => x.Id == villaId);
+        }
+    }
+}
